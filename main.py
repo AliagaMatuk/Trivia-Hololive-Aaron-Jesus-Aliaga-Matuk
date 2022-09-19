02913bf109dbe727ccb12aee8bc4f711c72e93b1
Aarón Jesús Jesús <31136900+AliagaMatuk@users.noreply.github.com>
2022-09-19 02:43:45 +0000

    Last version

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #Imports
 import time
+
+# Colores
+"""BLACK = '\033[30m' RED = '\033[31m' GREEN = '\033[32m' YELLOW = '\033[33m' BLUE = '\033[34m' CYAN = '\033[36m' RESET = '\033[39m'"""
+
 # Variables/texto
 intro = "\nBienvenido a mi trivia sobre la empresa de Vtubers: Hololive\n"
 presentacion = "Ingresa tu nombre: "
